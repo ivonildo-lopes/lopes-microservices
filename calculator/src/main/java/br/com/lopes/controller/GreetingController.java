edcd758c2a2605b5d93b4b33572dcec342f3225a
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2021-07-26 17:14:50 -0300

    initial commit

---
+++ calculator/src/main/java/br/com/lopes/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package br.com.lopes.controller;
+
+import br.com.lopes.entity.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController implements Serializable {
+
+
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    
+    @GetMapping(value = "/")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+
+        return new Greeting(counter.incrementAndGet(),String.format(TEMPLATE,name));
+    }
+}
---
+++ foo/src/main/java/br/com/lopes/entity/Greeting.java
@@ -0,0 +1,14 @@
+package br.com.lopes.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private long id;
+    private String content;
+}
